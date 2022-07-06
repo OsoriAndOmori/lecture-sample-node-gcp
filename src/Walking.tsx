5e073b44cb9dbd9bfb9a39fbbaccf9ddc8bc9e1c
OsoriAndOmori <skywhite15@korea.ac.kr>
2022-07-06 10:44:11 +0900

    react router 추가후 별도 view 분리

---
+++ src/Walking.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Walking = ()  => {
+  return (
+    <div className="walking">
+      <h2>React-Router Test</h2>
+    </div>
+  );
+}
+
+export default Walking;
